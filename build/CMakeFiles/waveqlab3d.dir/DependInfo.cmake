03d645eaa3a512c45fd02acbc5968433491b4cab
Fonglulu <fung_micro@outlook.com>
2020-06-25 22:39:29 +1000

    Fixed a bug in boundary upwind operator

---
+++ build/CMakeFiles/waveqlab3d.dir/DependInfo.cmake
@@ -0,0 +1,56 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "Fortran"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_Fortran
+  "/home/fred/Desktop/waveqlab_co/src/BoundaryConditions.f90" "/home/fred/Desktop/waveqlab_co/build/CMakeFiles/waveqlab3d.dir/BoundaryConditions.f90.o"
+  "/home/fred/Desktop/waveqlab_co/src/CouplingForcing.f90" "/home/fred/Desktop/waveqlab_co/build/CMakeFiles/waveqlab3d.dir/CouplingForcing.f90.o"
+  "/home/fred/Desktop/waveqlab_co/src/Interface_Condition.f90" "/home/fred/Desktop/waveqlab_co/build/CMakeFiles/waveqlab3d.dir/Interface_Condition.f90.o"
+  "/home/fred/Desktop/waveqlab_co/src/JU_xJU_yJU_z6.f90" "/home/fred/Desktop/waveqlab_co/build/CMakeFiles/waveqlab3d.dir/JU_xJU_yJU_z6.f90.o"
+  "/home/fred/Desktop/waveqlab_co/src/RHS_Interior.f90" "/home/fred/Desktop/waveqlab_co/build/CMakeFiles/waveqlab3d.dir/RHS_Interior.f90.o"
+  "/home/fred/Desktop/waveqlab_co/src/block.f90" "/home/fred/Desktop/waveqlab_co/build/CMakeFiles/waveqlab3d.dir/block.f90.o"
+  "/home/fred/Desktop/waveqlab_co/src/boundary.f90" "/home/fred/Desktop/waveqlab_co/build/CMakeFiles/waveqlab3d.dir/boundary.f90.o"
+  "/home/fred/Desktop/waveqlab_co/src/common.f90" "/home/fred/Desktop/waveqlab_co/build/CMakeFiles/waveqlab3d.dir/common.f90.o"
+  "/home/fred/Desktop/waveqlab_co/src/datatypes.f90" "/home/fred/Desktop/waveqlab_co/build/CMakeFiles/waveqlab3d.dir/datatypes.f90.o"
+  "/home/fred/Desktop/waveqlab_co/src/domain.f90" "/home/fred/Desktop/waveqlab_co/build/CMakeFiles/waveqlab3d.dir/domain.f90.o"
+  "/home/fred/Desktop/waveqlab_co/src/elastic.f90" "/home/fred/Desktop/waveqlab_co/build/CMakeFiles/waveqlab3d.dir/elastic.f90.o"
+  "/home/fred/Desktop/waveqlab_co/src/fault_output.f90" "/home/fred/Desktop/waveqlab_co/build/CMakeFiles/waveqlab3d.dir/fault_output.f90.o"
+  "/home/fred/Desktop/waveqlab_co/src/fields.f90" "/home/fred/Desktop/waveqlab_co/build/CMakeFiles/waveqlab3d.dir/fields.f90.o"
+  "/home/fred/Desktop/waveqlab_co/src/grid.f90" "/home/fred/Desktop/waveqlab_co/build/CMakeFiles/waveqlab3d.dir/grid.f90.o"
+  "/home/fred/Desktop/waveqlab_co/src/iface.f90" "/home/fred/Desktop/waveqlab_co/build/CMakeFiles/waveqlab3d.dir/iface.f90.o"
+  "/home/fred/Desktop/waveqlab_co/src/initial_stress_condition.f90" "/home/fred/Desktop/waveqlab_co/build/CMakeFiles/waveqlab3d.dir/initial_stress_condition.f90.o"
+  "/home/fred/Desktop/waveqlab_co/src/inter_material.f90" "/home/fred/Desktop/waveqlab_co/build/CMakeFiles/waveqlab3d.dir/inter_material.f90.o"
+  "/home/fred/Desktop/waveqlab_co/src/main.f90" "/home/fred/Desktop/waveqlab_co/build/CMakeFiles/waveqlab3d.dir/main.f90.o"
+  "/home/fred/Desktop/waveqlab_co/src/material.f90" "/home/fred/Desktop/waveqlab_co/build/CMakeFiles/waveqlab3d.dir/material.f90.o"
+  "/home/fred/Desktop/waveqlab_co/src/metrics.f90" "/home/fred/Desktop/waveqlab_co/build/CMakeFiles/waveqlab3d.dir/metrics.f90.o"
+  "/home/fred/Desktop/waveqlab_co/src/mms.f90" "/home/fred/Desktop/waveqlab_co/build/CMakeFiles/waveqlab3d.dir/mms.f90.o"
+  "/home/fred/Desktop/waveqlab_co/src/moment_tensor.f90" "/home/fred/Desktop/waveqlab_co/build/CMakeFiles/waveqlab3d.dir/moment_tensor.f90.o"
+  "/home/fred/Desktop/waveqlab_co/src/mpi3d_interface.f90" "/home/fred/Desktop/waveqlab_co/build/CMakeFiles/waveqlab3d.dir/mpi3d_interface.f90.o"
+  "/home/fred/Desktop/waveqlab_co/src/mpi3dbasic.f90" "/home/fred/Desktop/waveqlab_co/build/CMakeFiles/waveqlab3d.dir/mpi3dbasic.f90.o"
+  "/home/fred/Desktop/waveqlab_co/src/mpi3dcomm.f90" "/home/fred/Desktop/waveqlab_co/build/CMakeFiles/waveqlab3d.dir/mpi3dcomm.f90.o"
+  "/home/fred/Desktop/waveqlab_co/src/mpi3dio.f90" "/home/fred/Desktop/waveqlab_co/build/CMakeFiles/waveqlab3d.dir/mpi3dio.f90.o"
+  "/home/fred/Desktop/waveqlab_co/src/parallel_write.f90" "/home/fred/Desktop/waveqlab_co/build/CMakeFiles/waveqlab3d.dir/parallel_write.f90.o"
+  "/home/fred/Desktop/waveqlab_co/src/plastic.f90" "/home/fred/Desktop/waveqlab_co/build/CMakeFiles/waveqlab3d.dir/plastic.f90.o"
+  "/home/fred/Desktop/waveqlab_co/src/plastic_material.f90" "/home/fred/Desktop/waveqlab_co/build/CMakeFiles/waveqlab3d.dir/plastic_material.f90.o"
+  "/home/fred/Desktop/waveqlab_co/src/pml.f90" "/home/fred/Desktop/waveqlab_co/build/CMakeFiles/waveqlab3d.dir/pml.f90.o"
+  "/home/fred/Desktop/waveqlab_co/src/seismogram.f90" "/home/fred/Desktop/waveqlab_co/build/CMakeFiles/waveqlab3d.dir/seismogram.f90.o"
+  "/home/fred/Desktop/waveqlab_co/src/slice_output.f90" "/home/fred/Desktop/waveqlab_co/build/CMakeFiles/waveqlab3d.dir/slice_output.f90.o"
+  "/home/fred/Desktop/waveqlab_co/src/time_step.f90" "/home/fred/Desktop/waveqlab_co/build/CMakeFiles/waveqlab3d.dir/time_step.f90.o"
+  "/home/fred/Desktop/waveqlab_co/src/unit_normals.f90" "/home/fred/Desktop/waveqlab_co/build/CMakeFiles/waveqlab3d.dir/unit_normals.f90.o"
+  )
+set(CMAKE_Fortran_COMPILER_ID "GNU")
+set(CMAKE_Fortran_SUBMODULE_SEP "@")
+set(CMAKE_Fortran_SUBMODULE_EXT ".smod")
+
+# The include file search paths:
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH
+  "/usr/include/x86_64-linux-gnu/mpich"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
